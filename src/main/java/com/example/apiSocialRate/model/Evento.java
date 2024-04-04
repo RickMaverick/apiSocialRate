ae37a00578ba8c04b44c6a1052bcd2af678dbc32
ricardo-temple <ricardovtemple@gmail.com>
2024-04-04 19:51:04 -0300

    Update classes modelo

--- src/main/java/com/example/apiSocialRate/model/Evento.java
+++ src/main/java/com/example/apiSocialRate/model/Evento.java
@@ -1,11 +1,15 @@
 package com.example.apiSocialRate.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Evento {
+    @Id
+    @GeneratedValue
     private Long idEvento;
     private String nomeEvento;
 }
---
+++ src/main/java/com/example/apiSocialRate/repository/EventoRepository.java
@@ -0,0 +1,2 @@
+package com.example.apiSocialRate.repository;public interface EventoRepository {
+}
