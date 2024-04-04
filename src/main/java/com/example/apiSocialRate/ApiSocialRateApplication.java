a92c3928d32b56aaceecea460096c82fbd8181a0
ricardo-temple <ricardovtemple@gmail.com>
2024-04-04 17:31:36 -0300

    initial commit

---
+++ src/main/java/com/example/apiSocialRate/ApiSocialRateApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiSocialRate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSocialRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSocialRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiSocialRate/ApiSocialRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiSocialRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSocialRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
